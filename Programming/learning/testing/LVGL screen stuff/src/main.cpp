724ff12e432983f388b3601f084d2f562d42d997
Meerlikeskeer <66336457+Meerlikeskeer@users.noreply.github.com>
2024-03-26 14:07:13 -0700

    fixed it yay

--- Programming/learning/testing/LVGL screen stuff/src/main.cpp
+++ Programming/learning/testing/LVGL screen stuff/src/main.cpp
@@ -1,7 +1,57 @@
 #include "main.h"
 #include "liblvgl/lvgl.h"
 lv_color_t fun_green =  lv_color_make(0, 100, 46);
+lv_obj_t * tabview = lv_tabview_create(lv_scr_act(), LV_DIR_LEFT, 0);
+
 int updateBlud = 0;
+void set_tabs(){
+    lv_obj_set_style_bg_color(tabview, fun_green, 0);
+    lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tabview);
+    lv_obj_set_style_bg_color(tab_btns, fun_green, 0);
+    lv_obj_set_style_text_color(tab_btns, fun_green, 0);
+    lv_obj_align(tab_btns, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_set_width(tab_btns, 0);
+    lv_obj_set_height(tab_btns, 0);
+    lv_obj_set_style_bg_color(tabview, fun_green, 0);
+    /*Add 3 tabs (the tabs are page (lv_page) and can be scrolled*/
+    lv_obj_t * tab1 = lv_tabview_add_tab(tabview, LV_SYMBOL_GPS "  AUTOS");
+    lv_obj_t * tab2 = lv_tabview_add_tab(tabview, "Tab 2");
+    lv_obj_t * tab3 = lv_tabview_add_tab(tabview, "Tab 3");
+    lv_obj_t * label = lv_label_create(tab1);
+
+    /*Add content to the tabs*/
+    static lv_style_t tab_style;
+    lv_style_init(&tab_style);
+    lv_style_set_radius(&tab_style, 10);
+    lv_style_set_bg_opa(&tab_style, LV_OPA_COVER);
+    lv_style_set_bg_color(&tab_style, fun_green);
+    lv_style_set_border_color(&tab_style, lv_color_black());
+    lv_style_set_border_width(&tab_style, 2);
+	lv_style_set_text_color(&tab_style, lv_color_white());
+
+    lv_obj_add_style(tab1, &tab_style, 0);
+    lv_obj_add_style(tab2, &tab_style, 0);
+    lv_obj_add_style(tab3, &tab_style, 0);
+    
+    static lv_style_t text_style;
+    lv_style_init(&text_style);
+    lv_style_set_bg_opa(&tab_style, LV_OPA_COVER);
+    lv_style_set_border_color(&tab_style, lv_color_black());
+	lv_style_set_text_color(&tab_style, lv_color_white());
+
+    lv_obj_add_style(label, &text_style, 0);
+
+
+    lv_label_set_text(label, "First tab                      this is the first");
+
+    label = lv_label_create(tab2);
+    lv_label_set_text(label, "Second tab                      this is the second");
+
+    label = lv_label_create(tab3);
+    lv_label_set_text(label, "Third tab                      this is the third");
+
+}
+
 void lv_example_menu_1(void) {
     /*Create a menu object*/
     lv_obj_t * menu = lv_menu_create(lv_scr_act());
@@ -49,65 +99,24 @@ void lv_example_menu_1(void) {
             lv_menu_set_page(menu, sensors_sub_page);
     }
 }
-void selector_dropdown(void){
-
+static void event_handler(lv_event_t * e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * obj = lv_event_get_target(e);
+    if(code == LV_EVENT_VALUE_CHANGED) {
+        char buf[32];
+        LV_LOG_USER("Option: %s", buf);
+        if (lv_dropdown_get_selected(obj) == 0){
+            lv_tabview_set_act(tabview, 0, LV_ANIM_ON);
+        } else if (lv_dropdown_get_selected(obj) == 1){
+            lv_tabview_set_act(tabview, 1, LV_ANIM_ON);
+        } else if (lv_dropdown_get_selected(obj) == 2 ){
+            lv_tabview_set_act(tabview, 2, LV_ANIM_ON);
+        }
+    }
 }
 
-
-bool auto_started = false;
-
-
-
-void initialize()
+void lv_example_dropdown_1(void)
 {
-    /*Create a Tab view object*/
-    lv_obj_t * tabview;
-    tabview = lv_tabview_create(lv_scr_act(), LV_DIR_LEFT, 80);
-    lv_obj_set_style_bg_color(tabview, fun_green, 0);
-    lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tabview);
-    lv_obj_set_style_bg_color(tab_btns, fun_green, 0);
-    lv_obj_set_style_text_color(tab_btns, fun_green, 0);
-    lv_obj_align(tab_btns, LV_ALIGN_TOP_LEFT, 20, 20);
-    lv_obj_set_width(tab_btns, 150);
-    lv_obj_set_height(tab_btns, 400);
-    lv_obj_set_style_bg_color(tabview, fun_green, 0);
-    /*Add 3 tabs (the tabs are page (lv_page) and can be scrolled*/
-    lv_obj_t * tab1 = lv_tabview_add_tab(tabview, LV_SYMBOL_GPS "  AUTOS");
-    lv_obj_t * tab2 = lv_tabview_add_tab(tabview, "Tab 2");
-    lv_obj_t * tab3 = lv_tabview_add_tab(tabview, "Tab 3");
-    lv_obj_t * label = lv_label_create(tab1);
-
-    /*Add content to the tabs*/
-    static lv_style_t tab_style;
-    lv_style_init(&tab_style);
-    lv_style_set_radius(&tab_style, 10);
-    lv_style_set_bg_opa(&tab_style, LV_OPA_COVER);
-    lv_style_set_bg_color(&tab_style, fun_green);
-    lv_style_set_border_color(&tab_style, lv_color_black());
-    lv_style_set_border_width(&tab_style, 2);
-	lv_style_set_text_color(&tab_style, lv_color_white());
-
-    lv_obj_add_style(tab1, &tab_style, 0);
-    lv_obj_add_style(tab2, &tab_style, 0);
-    lv_obj_add_style(tab3, &tab_style, 0);
-    
-    static lv_style_t text_style;
-    lv_style_init(&text_style);
-    lv_style_set_bg_opa(&tab_style, LV_OPA_COVER);
-    lv_style_set_border_color(&tab_style, lv_color_black());
-	lv_style_set_text_color(&tab_style, lv_color_white());
-
-    lv_obj_add_style(label, &text_style, 0);
-
-
-    lv_label_set_text(label, "First tab                      this is the first");
-
-    label = lv_label_create(tab2);
-    lv_label_set_text(label, "Second tab                      this is the second");
-
-    label = lv_label_create(tab3);
-    lv_label_set_text(label, "Third tab                      this is the third");
-
     static lv_style_t style;
     lv_style_init(&style);
     lv_style_set_radius(&style, 10);
@@ -127,29 +136,17 @@ void initialize()
     lv_dropdown_set_symbol(dd1, &LV_SYMBOL_SETTINGS);	
 	lv_dropdown_set_selected_highlight(dd1, true);
     lv_obj_align(dd1, LV_ALIGN_TOP_LEFT, 20, 20);
-    // if (lv_dropdown_get_selected(dd1) == 0){
-    //     printf("hello1");
-    //     lv_tabview_set_act(tabview, 0, LV_ANIM_OFF);
-    // } else if (lv_dropdown_get_selected(dd1) == 1 ){
-    //     printf("hello2");
-    //     lv_tabview_set_act(tabview, 1, LV_ANIM_OFF);
-    // } else if (lv_dropdown_get_selected(dd1) == 2){
-    //     printf("hello3");
-    //     lv_tabview_set_act(tabview, 2, LV_ANIM_OFF);
-    // } 
-    switch(lv_dropdown_get_selected(dd1)){
-        case 0: 
-            lv_tabview_set_act(tabview, 0, LV_ANIM_ON);
-            break;
-        case 1: 
-            lv_tabview_set_act(tabview, 1, LV_ANIM_ON);
-            break;
-        case 2: 
-            lv_tabview_set_act(tabview, 2, LV_ANIM_ON);
-            break;
-        case 4: 
-            break;
-    }
+    lv_obj_add_event_cb(dd1, event_handler, LV_EVENT_ALL, NULL);
+}
+
+
+bool auto_started = false;
+
+
+
+void initialize() {
+    set_tabs();
+    lv_example_dropdown_1();
 }
 
 
